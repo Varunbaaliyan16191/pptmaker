cb30350ff094e07ff2a5047157a734328b6ba943
Varunbaaliyan16191 <varunbaaliyan123456789@gmail.com>
2026-07-31 13:27:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-# step 1 load modules 
+# ==========Load Module==========
+
 import os
 import time
 import langchain
@@ -7,154 +8,202 @@ from langchain_groq import ChatGroq
 from langchain_google_genai import ChatGoogleGenerativeAI
 import pytesseract as pyt
 from tavily import TavilyClient
-from langchain.messages import SystemMessage, HumanMessage 
+from langchain.messages import SystemMessage, HumanMessage
 import numpy as np
 import streamlit as st
 
-#=========================== STEP 2 =============================
-st.set_page_config(layout = "wide")
-st.title("AI PPT GENERATOR")
+# ==========Streamlit Front-End==========
+
+# to show web-app: complete page layout
+st.set_page_config(layout="wide")
+
+st.title("AI PPT Generator")
 st.divider()
-st.sidebar.title("ENTER API-KEYS")
+st.sidebar.title("Enter API Key's")
 
-#========step 3 ====================
+# ==========Load API Key==========
 
-GOOGLE_API_KEY = st.sidebar.text_input("GOOGLE_API_KEY", type = "password")
-TAVILY_API_KEY = st.sidebar.text_input("TAVILY_API_KEY", type = "password")
+GOOGLE_API_KEY = st.sidebar.text_input("Google-API", type = "password")
+TAVILY_API_KEY = st.sidebar.text_input("Tavily-API", type = "password")
 
+# ==========API Validations==========
 
-#============API VALIDATIONS ============
-ALL_API = [GOOGLE_API_KEY,TAVILY_API_KEY]
+ALL_API = [GOOGLE_API_KEY, TAVILY_API_KEY]
 
 if not all(ALL_API):
-  st.sidebar.error("MUST PASS ALL API KEYS")
+  st.sidebar.error("Must Pass all API Key's")
+
 elif all(ALL_API):
-  st.sidebar.success("API_KEY loaded")
-  #MODEL LOAD 
+  st.sidebar.success("API KEY's Loaded Successfully")
+
+  # Model Load
   model = ChatGoogleGenerativeAI(
     google_api_key = GOOGLE_API_KEY,
     model = st.sidebar.selectbox("Gemini-Model-Name",
-                                 options = ["gemini-2.5-flash",
-                                            "gemini-2.5-flash-lite",
-                                            "gemini-3.5-flash,
-                                            "gemini-3.5-flash-lite"])
+                                 options = ['gemini-2.5-flash',
+                                            'gemini-2.5-flash-lite',
+                                            'gemini-3.5-flash',
+                                            'gemini-3.5-flash-lite'])
   )
 else:
-  st.sidebar.info("check api keys")
+  st.sidebar.info("Check API Key's")
 
-# search the latest info using tavily
-def search_latest_info(query):
+# ==========Back-End code==========
+
+# Tool 1:
+# Search_latest_info using tavily
+
+def Search_latest_info(query):
   """this function helps to give
-  latest search info using tavily
+  latest search using tavily
   based on given user query related research
-  or contents"""
-  client = TavilyClient(TAVILY_API_KEY)
+  or contant"""
+
+  client = TavilyClient(api_key = TAVILY_API_KEY)
   response = client.search(query)
   return response
 
-#==============step 6 user input ====================
-st.header("write the prompt to generate ppt or image or fetch latest news")
-iser_input = st.text_area("write here: ")
+# ==========User Input==========
 
-def generate_image(img_prompt,slide_no = 1):
-  """this function helps user generate
-  image using free api key with given
-  image_prompt"""
+st.header("Write prompt to Generate PPT or Image or fetch Latest News")
+
+user_input = st.text_area("Write Here: ")
+
+
+
+# Tool 2:
+# Generate image using free api
+
+def generate_image(img_prompt, slide_no = 1):
+  """this function helps user to generate
+  image using free api, with given
+  img_promot"""
+
   url = f"https://image.pollinations.ai/{img_prompt}"
 
   import requests as r
   content = r.get(url).content
   with open(f"ai_image_{slide_no}.jpeg",'wb') as f:
     f.write(content)
+
   from PIL import Image
   img = Image.open(f"ai_image_{slide_no}.jpeg")
   return url
 
+
+
+# Tool 3:
+
 def agent_prompt(query):
-  """this function helps promptify the given user
-  query, suppose user needs ppt based on given qury by user it gives detailed professional prompt
-  to return the output"""
+  """this function to promptify the given user
+  query, suppose user needs ppt based on given
+  query by user, it give detailed professional
+  prompt to return the prompt"""
 
-  prompt = f"""give high professional prompt for below given prompt :-
+  prompt = f"""give detailed highly professiional
+  prompt for below given prompt.
 
-  you are a professional ppt designer based on user given query, your task is to create
-  professional html output prompt with no markdown
-  user query :- {query}"""
+  you are professional ppt designer,
+  based on user given query, your task is to professional
+  HTML output prompt with no markdown.
+  User query: {query}"""
 
-  resonse = model.invoke(prompt)
-  final_prompt = resonse.content[-1]['text']
+  response = model.invoke(prompt)
+  final_prompt = response.content[-1]['text']
 
-  with open("PPT_PROMPT.txt", 'w') as f:
+  with open("PPT_prompt.txt", 'w') as f:
     f.write(final_prompt)
+
   return final_prompt
 
-def run_agent(leader_agent, query):
-  prompt = f"""Based on Below given Query, your task is to call specific tool,
-  first to promptify user prompt, than call image tool, or latest search if
-  required.give slide dynamic, ui ux, with creative design, keep help of
-  function to generate image based on given topic, Generate image using
-   with no of slide asked
-  and imbed that in same html ppt and using file handling embed this in
-  output html, use java script function to generate image using async
-  func and threading and give output in HTML user query given below:"""
-  prompt += query
+
+
+# Tool 4:
+
+def run_agent(leader_agent,query):
+  prompt = f"""Based on below given query,
+  your task is to call specific tool, first to
+  promptify user prompt, than call image tool, or
+  latest search if required. give slide dynamic, ui ux,
+  with creative design,keep help of function to generate image
+  based on given topic,
+  Generate image using
+  with no of slide asked
+  and imbed that in same html ppt
+  and using file handling embed this in output html,
+  use java script function
+  to generate image using async func and threading
+  and give output in HTML
+  user query given below:
+
+  """
+  prompt+= query
   prompt = agent_prompt(prompt)
-  response = leader_agent.invoke({'messages': [{'role': 'user', 'content': prompt}]})
-  code = response['messages'] [-1].content[-1]['text']
+  
+  response = leader_agent.invoke({'messages':[{'role':'user','content':prompt}]})
+  code = response['messages'][-1].content[-1]['text']
   return code
 
-#==================step 7 ========================
-leader_agent = create_agent(
-    model = model,
-    tools = [search_latest_info,
-             #generate_image
-             ])
+# ==========Agent Call==========
 
+# leader_agent creation
+if all(ALL_API):
+  leader_agent = create_agent(
+      model = model,
+      tools = [Search_latest_info,generate_image]
+  )
+else:
+  st.info("Pass All API Key's and Return")
+# ==========Navbar Streamlit=========
 
-#======================step no 8========================
-tab1,tab2,tab3 = st.tabs(["Generate Image",
-                          "Fetch latest news",
-                          "Generate PPT"])
+tab1, tab2, tab3 = st.tabs(["GENERATE IMAGE", 
+                            "FETCH LATEST NEWS",
+                            "GENERATE PPT"])
 
 if (user_input) and (leader_agent):
-  #tab 1 code 
+  # TAB 1:
   with tab1:
-   if st.button("Generate image",key = "Gen-Image"):
-     with st.spinner("Running agent"):
-       try:
-         img = generate_image(user_input)
-         st.image(img)
+    if st.button("Click to generate: ", key="generate_img_button"):
+      with st.spinner("Running Agent.."):
+        try:
+          img = generate_image(user_input)
+          st.image(img)
         except:
           url = f"https://image.pollinations.ai/{user_input}"
           time.sleep(4)
           st.image(url)
+          
+  # TAB 2:        
   with tab2:
-    if st.button("Fetch News", key = "Fetch-News"):
-      with st.spinner("Running Agent"):
+    if st.button("Fetch news: ", key="news_button"):
+      with st.spinner("Running Agent.."):
         try:
-          prompt = "Give Multiple news in HTML card Format for topic" + user_input
-          response = leader_agent.invoke({'messages':[{'role': 'user',
-                                                      'content': prompt}]})
-          code = response['messages'] [-1].content[-1]['text']
-          st.html(code, width="stretch",
-          unsafe_allow_javascript=True)
+          prompt = "Give Multiple news in HTML card format for topic" + user_input
+        
+          response = leader_agent.invoke({'messages':[{'role':"user", "content":prompt}]})
+          code = response['messages'][-1].content[-1]['text']
+  
+          st.html(code, width="stretch", unsafe_allow_javascript=True)
         except Exception as err:
           st.error(err)
+
+  # TAB 3:
   with tab3:
-    if st.button("Generate PPT", key = "Gen-PPT"):
-      with st.spinner("Running Agent"):
+    if st.button("Click to generate: ", key="generate_ppt_button"):
+      with st.spinner("Running Agent.."):
         try:
-          code run_agent(leader_agent, user)
-          st.html(code, width="stretch",
-          unsafe_allow_javascript=True)
-          #file savce
+          code = run_agent(leader_agent, user_input)
+          st.html(code, width="stretch", unsafe_allow_javascript=True)
+
+          # FILE Save
           with open("ppt.html",'w') as f:
             f.write(code)
           st.download_button(label = "DOWNLOAD PPT",
-                          data code,
+                          data = code,
                           file_name = 'ppt.html',
                           mime = 'text/html')
         except Exception as err:
           st.error(err)
+
 else:
-  st.error("something went wrong")
+  st.error("Something went Wrong!!")
